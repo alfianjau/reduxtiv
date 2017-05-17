e6de13e110c901b19925e5a0e12d836a4e2836e1
alfianjau <alfianjauhari@gmail.com>
2017-05-17 17:06:01 +0700

    structure: add logo

--- src/components/sub/Molecule/Nav/Nav.js
+++ src/components/sub/Molecule/Nav/Nav.js
@@ -1,27 +1,32 @@
 import React from 'react'
 // import Card from '../../Atom/Card'
 import { IndexLink, Link } from 'react-router'
-import { Button } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import './nav.scss'
 
 export const Navbar = () => (
   <div className='Navbar'>
-    <div className='container text-right' >
-      <IndexLink to='/' activeClassName='route--active'>
-        <Button basic inverted>Home</Button>
-      </IndexLink>
-      <Link to='/counter' activeClassName='route--active'>
-        <Button basic inverted>About-us</Button>
-      </Link>
-      <Link to='/counter' activeClassName='route--active'>
-        <Button basic inverted>Counter</Button>
-      </Link>
-      <Link to='/counter' activeClassName='route--active'>
-        <Button basic inverted>Our Work</Button>
-      </Link>
-      <Link to='/counter' activeClassName='route--active'>
-        <Button basic inverted>Contact</Button>
-      </Link>
+    <div className='container' >
+      <div className='logo text-left'>
+        <Icon inverted name='copyright' size='huge' />
+      </div>
+      <div className='nav text-right'>
+        <IndexLink to='/' activeClassName='route--active'>
+          <Button basic inverted>Home</Button>
+        </IndexLink>
+        <Link to='/counter' activeClassName='route--active'>
+          <Button basic inverted>About-us</Button>
+        </Link>
+        <Link to='/counter' activeClassName='route--active'>
+          <Button basic inverted>Counter</Button>
+        </Link>
+        <Link to='/counter' activeClassName='route--active'>
+          <Button basic inverted>Our Work</Button>
+        </Link>
+        <Link to='/counter' activeClassName='route--active'>
+          <Button basic inverted>Contact</Button>
+        </Link>
+      </div>
     </div>
   </div>
 )
